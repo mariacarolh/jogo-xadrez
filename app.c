39c613ea3309f15c7d2144c2589dfcdf83fcda58
mariacarolh <mcp.carolina2020@gmail.com>
2024-05-14 20:42:44 -0300

    Criando peças

--- app.c
+++ app.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #define TAM_TABULEIRO 8 // Tamanho padrão do tabuleiro
 
+// Peças pretas
+const char pP = 'P'; // Peao
+const char pT = 'T'; // Torre
+const char pC = 'C'; // Cavalo
+const char pB = 'B'; // Bispo
+const char pD = 'D'; // Dama
+const char pR = 'R'; // Rei
+
+const char V = ' '; // Espaço vazio
+
+// Peças brancas
+const char bP = 'P'; // Peao
+const char bT = 'T'; // Torre
+const char bC = 'C'; // Cavalo
+const char bB = 'B'; // Bispo
+const char bD = 'D'; // Dama
+const char bR = 'R'; // Rei
+
 char tabuleiro[TAM_TABULEIRO][TAM_TABULEIRO]; // Definir tamanho do tabuleiro 8 por 8
 
 void exibirTabuleiro()
